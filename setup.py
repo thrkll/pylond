b80fc8f3762dc29fe92d4114fb4ea599ab454320
thkrll <thorkell@thorkell.lv>
2022-11-04 16:43:31 +0000

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="pylond",
+    version="0.0.1",
+    author="Þorkell Einarsson",
+    author_email="thorkell@thorkell.lv",
+    description="",
+    license="MIT",
+    url="https://github.com/thrkll/pylond",
+    install_requires=[], 
+)
