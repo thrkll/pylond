61d08f42df096081ccb5b32e8f71263cb4424253
thrkll <thorkell@thorkell.lv>
2022-11-15 16:46:38 +0000

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
-    name="pylond",
-    version="0.0.1",
-    author="Þorkell Einarsson",
-    author_email="thorkell@thorkell.lv",
-    description="",
-    license="MIT",
-    url="https://github.com/thrkll/pylond",
-    install_requires=[], 
-)
+    name = 'pylond',
+    version = '1.0.0',
+    author = 'Þorkell Einarsson',
+    author_email = 'thorkell@thorkell.lv',
+    description = 'pylond provides translations of countries and nationalities into Icelandic',
+    long_description = long_description
+    long_description_content_type = 'text/markdown'
+    license = 'MIT',
+    url = 'https://github.com/thrkll/pylond',
+    install_requires = [numpy>=1.19.0])
