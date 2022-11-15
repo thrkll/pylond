15e15a62dfe8abc82d0e632802061b0137436230
thrkll <thorkell@thorkell.lv>
2022-11-15 15:59:29 +0000

    Added docstrings and sort argument for iter_countries)

--- pylond/pylond.py
+++ pylond/pylond.py
@@ -1,11 +1,8 @@
 import csv
 import numpy as np
 
-# TODO:
-#   Búa til functions langs_iter() countries_iter() sem iterate og skila í
-#   stafrófsröð
-
 def open_data():
+    '''Opens data to be utilized by other functions.'''
     with open('pylond/data/country_data.csv', 'r', encoding = 'utf8') as file:
         reader = csv.reader(file)
         for row in reader:
@@ -15,6 +12,7 @@ countries = open_data()
 headers = next(countries)
 
 def alpha2(alpha2) -> str:
+    '''Return a country based on the ISO-3166-1 alpha2 code.'''
     result = {}
     for row in countries:
         if row[1].lower() == alpha2.lower():
@@ -26,6 +24,7 @@ def alpha2(alpha2) -> str:
     return result
 
 def alpha3(alpha3) -> str:
+    '''Return a country based on the ISO-3166-1 alpha3 code.'''
     result = {}
     for row in countries:
         if row[2].lower() == alpha3.lower():
@@ -37,6 +36,7 @@ def alpha3(alpha3) -> str:
     return result
 
 def numeric(numeric)-> int:
+    '''Return a country based on the ISO-3166-1 numeric code.'''
     result = {}
     for row in countries:
         if int(row[3]) == numeric:
@@ -48,7 +48,8 @@ def numeric(numeric)-> int:
     return result
 
 def from_english(english_name, lev_ratio=1) -> str:
-    result = [] # Multiple results are possible
+    '''Search for a country based on the English short name. Optional lev_ratio parameter should be in range of 0 to 1 (default).'''
+    result = []
     for row in countries:
         ratio = levenshtein_ratio(row[0].lower(), english_name.lower())
         if ratio >= lev_ratio:
@@ -63,7 +64,7 @@ def from_english(english_name, lev_ratio=1) -> str:
     return result
 
 def levenshtein_ratio(s, t):
-    # Only used by from_english()
+    '''Calculates levenshtein distance and ratio for from_english().'''
     rows = len(s)+1
     cols = len(t)+1
     distance = np.zeros((rows,cols),dtype = int)
@@ -83,7 +84,11 @@ def levenshtein_ratio(s, t):
     ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
     return ratio
 
-def iter_countries():
+def iter_countries(sort='eng') -> str:
+    '''Returns a list of countries. The sort parameter can take "eng" (default) or "isl"'''
+    valid = ['eng', 'isl']
+    if sort not in valid:
+        raise ValueError('Sort parameter can only take "eng" or "isl"')
     result = []
     for row in countries:
         inner_dict = {}
@@ -93,4 +98,6 @@ def iter_countries():
                 row.remove(value)
                 break
         result.append(inner_dict)
+    if sort == valid[1]:
+        result = sorted(result, key = lambda d: d['short_name'])
     return result
